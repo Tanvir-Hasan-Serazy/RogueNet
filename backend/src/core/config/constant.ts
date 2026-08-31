6c679bf61644127a0f599e7b87ec2b3164c40d12
Tanvir Hasan Serazy <thserazy@gmail.com>
2026-09-01 03:08:17 +0600

    feat: Update package dependencies and add environment variables for file upload and rate limiting

---
+++ backend/src/core/config/constant.ts
@@ -0,0 +1,3 @@
+export const MAX_FILE_SIZE = 5 * 1024 *1024
+export const CLOUDINARY_FOLDER = "roguenet/";
+
--- backend/src/core/config/env.ts
+++ backend/src/core/config/env.ts
@@ -4,4 +4,11 @@ dotenv.config();
 
 export const env = {
   PORT: Number(process.env.PORT) || 5000,
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+  JWT_RFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  BCRYPT_ROUNDS: process.env.BCRYPT_ROUNDS,
+  RATE_LIMIT: process.env.RATE_LIMIT,
 };
